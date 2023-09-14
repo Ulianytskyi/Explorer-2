29586597018596702edaae0516241fffd57e3b41
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-09-14 17:24:56 +0300

    Commit 7

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,8 @@ if (window.innerWidth <= 700) {
     canvas.classList.add('hide');
 }
 
+sizeOfScreen = sizeOfScreen - ((sizeOfScreen / 100) * 10);
+
 gameContainer.style.width = sizeOfScreen + 'px';
 gameContainer.style.height = sizeOfScreen + 'px';
 gridsField.style.width = sizeOfScreen + 'px';
@@ -57,14 +59,14 @@ let coordsArray = [
     {top: step * 7, left: step * 8, height: step * 1, width: step * 1}
 ];
 
-createCollider (wallArray[0], coordsArray[0]);
-createCollider (wallArray[1], coordsArray[1]);
-createCollider (wallArray[2], coordsArray[2]);
-createCollider (wallArray[3], coordsArray[3]);
-createCollider (wallArray[4], coordsArray[4]);
-createCollider (wallArray[5], coordsArray[5]);
-createCollider (wallArray[6], coordsArray[6]);
-createCollider (wallArray[7], coordsArray[7]);
+// createCollider (wallArray[0], coordsArray[0]);
+// createCollider (wallArray[1], coordsArray[1]);
+// createCollider (wallArray[2], coordsArray[2]);
+// createCollider (wallArray[3], coordsArray[3]);
+// createCollider (wallArray[4], coordsArray[4]);
+// createCollider (wallArray[5], coordsArray[5]);
+// createCollider (wallArray[6], coordsArray[6]);
+// createCollider (wallArray[7], coordsArray[7]);
 
 // items on screen --------------------------------------------
 
@@ -88,13 +90,14 @@ function createItem (itemsField, name, icon, left, top) {
     coords[0] = name;
     coords[1] = step * left;
     coords[2] = step * top;
+    coords[3] = icon;
 
     return coords;
 }
 
 let objectsArray = [];
-objectsArray.push(createItem(itemsField, 'Apple', '🍎', 2, 0));
-objectsArray.push(createItem(itemsField, 'Apple', '🍎', 4, 0));
+objectsArray.push(createItem(itemsField, 'Apple', '🍎', 9, 1));
+objectsArray.push(createItem(itemsField, 'Apple', '🍎', 0, 1));
 objectsArray.push(createItem(itemsField, 'Bill', '👨🏻‍🦰', 5, 5));
 objectsArray.push(createItem(itemsField, 'House', '🏠', 5, 8));
 
@@ -102,10 +105,36 @@ export function takeObjectsArray() {
     return objectsArray;
 }
 
+const playerView = document.createElement('div');
+
 export function checkItem (x, y, array) {
+    displayPlayerView()
+    playerView.textContent = '';
     array.forEach(element => {
         if (x == element[1] && y == element[2]) {
-            console.log(element[0]);
+            playerView.classList.remove('hide');
+
+            playerView.textContent = object.textContent;
+            // playerView.textContent += ' ';
+            playerView.textContent += element[3];
+            
+            if (x == 0) {
+                playerView.style.left = 0 + 'px';
+            } else if (x >= 430) {
+                playerView.style.left = -step + 'px';
+            } else {
+                playerView.style.left = -step/2 + 'px';
+            }
         }
     });
+    
+}
+
+function displayPlayerView() {
+    playerView.classList.add('hide');
+    playerView.classList.add('player-view');
+    playerView.style.width = step * 2 + 'px';
+    playerView.style.height = step + 'px';
+    playerView.style.top = -step/100 + 'px';
+    object.appendChild(playerView);
 }
